649b23fab0f991b93279a91c6e035eee8d4723c8
Indik <indik47@gmail.com>
2017-10-27 22:45:36 +0300

    +add homework3 task 2 "calculating floor and entrance number based on flat number" v1

---
+++ src/com/gmail/indik47/homework3/BuildingFloorsEntrances.java
@@ -0,0 +1,44 @@
+package com.gmail.indik47.homework3;
+
+import java.util.Scanner;
+/*      2) Есть девятиэтажный дом, в котором 4 подъезда. Номер подъезда
+        начинается с единицы. На одном этаже 4 квартиры. Напишите программу
+        которая получит номер квартиры с клавиатуры, и выведет на экран на
+        каком этаже, какого подъезда расположенна эта квартира. Если такой
+        квартиры нет в этом доме то нужно сообщить об этом пользователю.        */
+
+public class BuildingFloorsEntrances {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int flatNumber;
+        int floor = 1;
+        int entrance = 1;
+
+        System.out.println("Enter flat number: ");
+        while (true) {
+            flatNumber = sc.nextInt();
+            if ((flatNumber < 1) || (flatNumber > 144)) {
+                System.out.println("This flat number doesn`t exist in this house. Pls re-renter flat number: ");
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < 144; i += 36) {
+            if (flatNumber > (i + 36)) {
+                entrance++;
+            }
+        }
+        for (int i = 0; i < 36; i += 4) {
+            if ((flatNumber % 36) == 0) {
+                floor = 9;
+                break;
+            } else if ((flatNumber % 36) > (i + 4)) {
+                floor++;
+            }
+        }
+        System.out.println("Entrance " + entrance);
+        System.out.println("Floor " + floor);
+
+    }
+}
