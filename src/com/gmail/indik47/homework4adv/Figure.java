1a56dcbd99ae709461082df9d388a7ac14b4c3d8
Indik <indik47@gmail.com>
2017-11-01 08:09:15 +0200

    +add Figure v2

--- src/com/gmail/indik47/homework4adv/Figure.java
+++ src/com/gmail/indik47/homework4adv/Figure.java
@@ -7,19 +7,23 @@ public class Figure {
         Scanner sc = new Scanner(System.in);
         int i = 1;
         boolean toggle = true;
+        String s = "";
         System.out.println("Enter height: ");
         int height = sc.nextInt();
 
-        for (;i > 0;) {
-            String s = "*******************";
-            System.out.println(s.substring(i, i * 2));
+        for (; i > 0; ) {
             if (i == height) {   //если счетчик дошел до макс.высоты, переключаем toggle в false и счетчик начинает идти в минус
                 toggle = false;
+                s = s + "*";    //при переключении toggle печатаем центральную, самую длинную строку
+                System.out.println(s);
             }
             if (toggle) {
                 i++;
+                s = s + "*";
+                System.out.println(s);
             } else {
                 i--;
+                System.out.println(s.substring(0, i));
             }
         }
     }
