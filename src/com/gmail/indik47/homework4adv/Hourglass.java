534dd1ada3ac42936cd129374d7d76f3d6fa16a0
Indik47 <indik47@gmail.com>
2017-11-02 16:58:57 +0200

    add homework4 adv Hourglass

--- src/com/gmail/indik47/homework4adv/Hourglass.java
+++ src/com/gmail/indik47/homework4adv/Hourglass.java
@@ -7,33 +7,23 @@ import java.util.Scanner;
 public class Hourglass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = 0;
+        int n = 0;
 
-        while ((num - 1) % 2 != 0) {
+        while (n % 2 == 0) {
             System.out.println("Enter width(odd number): ");
-            num = sc.nextInt();
+            n = sc.nextInt();
         }
 
-        for (int i = 0; i < num; i += 2) {    //верхняя половина песочных часов
-            for (int j = 0; j < i / 2; j++) { //печатаем пробелы
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < (n/2 - Math.abs(n/2 - i)); j++) {
                 System.out.print(" ");
             }
-            for (int k = num; k > i; k--) {   //печатаем звездочки
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-        for (int i = num; i > 0; i -= 2) {      //нижняя половина песочных часов
-            if (i == num) continue;             //пропускаем печать первой итерации (одна звезда)
-            for (int j = 0; j < i / 2; j++) {   //печатаем пробелы
-                System.out.print(" ");
-            }
-            for (int k = num; k >= i; k--) {    //печатаем звездочки
+            for (int k = 0; k  < (Math.abs(n - (2 * i+1))+1); k++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
     }
 }
-
 
