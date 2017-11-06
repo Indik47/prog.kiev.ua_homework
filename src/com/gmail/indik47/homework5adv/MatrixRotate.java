d0f566012580edd26b8efa4ab2eb9f5ca7311bc9
Indik47 <indik47@gmail.com>
2017-11-06 12:17:39 +0200

    add homework4 adv Figure

--- src/com/gmail/indik47/homework5adv/MatrixRotate.java
+++ src/com/gmail/indik47/homework5adv/MatrixRotate.java
@@ -4,13 +4,25 @@ package com.gmail.indik47.homework5adv;
         выполнении задания использовать дополнительный массив нельзя). В
         примере показан поворот на 90 градусов - (3 часа)*/
 
+import java.util.Scanner;
+
 public class MatrixRotate {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[][] arr = arrayInit(6);
-
         arrayPrint(arr);
-        arr = arrayRotate(arr);
-        System.out.println("-----------");
+        while (true) {
+            System.out.println("Enter 90, 180 or 270 degrees: ");
+            int rotate = sc.nextInt();
+            if (rotate == 90 || rotate == 180 || rotate == 270) {
+                for (int i = 0; i < rotate / 90; i++) {
+                    arrayRotateClock(arr);
+                }
+                break;
+            } else {
+                System.out.println("Wrong input.");
+            }
+        }
         arrayPrint(arr);
     }
 
@@ -22,7 +34,7 @@ public class MatrixRotate {
         return arr;
     }
 
-    public static int[][] arrayRotate(int[][] arr) {  //переворот массива на 90 по часовой стрелке
+    public static int[][] arrayRotateClock(int[][] arr) {  //переворот массива на 90 по часовой стрелке
         int m = arr.length;
         for (int i = 0; i < m / 2; i++) {
             for (int j = i; j < m - 1 - i; j++) {
@@ -31,6 +43,20 @@ public class MatrixRotate {
                 arr[m - 1 - j][i] = arr[m - 1 - i][m - 1 - j];
                 arr[m - 1 - i][m - 1 - j] = arr[j][m - 1 - i];
                 arr[j][m - 1 - i] = temp;
+            }
+        }
+        return arr;
+    }
+
+    public static int[][] arrayRotateCountClock(int[][] arr) {  //переворот массива на 90 против часовой стрелки
+        int m = arr.length;
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = i; j < m - 1 - i; j++) {
+                int temp = arr[j][j];
+                arr[i][j] = arr[j][m - 1 - i];
+                arr[j][m - 1 - i] = arr[m - 1 - i][m - 1 - j];
+                arr[m - 1 - i][m - 1 - j] = arr[m - 1 - j][i];
+                arr[m - 1 - j][i] = temp;
             }
         }
         return arr;
