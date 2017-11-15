d9d2764d87f73a405dbf2f8ccee67ec92ef85a35
Indik47 <indik47@gmail.com>
2017-11-15 18:48:23 +0200

    add homework8 adv EnglishTextFromFile

--- src/com/gmail/indik47/homework8adv/EnglishTextFromFile.java
+++ src/com/gmail/indik47/homework8adv/EnglishTextFromFile.java
@@ -2,14 +2,16 @@ package com.gmail.indik47.homework8adv;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class EnglishTextFromFile {
     public static void main(String[] args) {
         File file = new File("english.txt");
         String s = readTextFromFile(file);
-        System.out.println(s);
-        countLetters(s);
+        HashMap<Character, Integer> hm = countLettersInString(s.toLowerCase().toCharArray());
+        printCountedLetters(hm);
     }
 
     public static String readTextFromFile(File file) {
@@ -24,28 +26,37 @@ public class EnglishTextFromFile {
         return sb.toString();
     }
 
-    public static void countLetters(String s) {
-        int[] counts = new int[s.length()];
-        s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            int count = 0;
-            char ch = s.charAt(i);
-            if (!sb.toString().contains(ch+"")){
-                sb.append(ch);
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == ch) {
-                    count++;
+    public static HashMap<Character, Integer> countLettersInString(char[] s) {
+        int counter;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            counter = 0;
+            for (char ch2 : s) {
+                if (ch2 == ch) {
+                    counter++;
                 }
             }
-            counts[i] = count;
-        }}
-        for (int i : counts) {
-            System.out.print(i + " ");
+            hm.put(ch, counter);
         }
-
-
+        return hm;
     }
 
+    public static void printCountedLetters(HashMap<Character, Integer> hm) {
+        int maxValue = 0;
+        for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+            Integer value = entry.getValue();
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        for (int i = maxValue; i > 0; i--) {
+            for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+                Integer value = entry.getValue();
+                Character key = entry.getKey();
+                if (value == i) {
+                    System.out.println(key + ": " + value);
+                }
+            }
+        }
+    }
 }
